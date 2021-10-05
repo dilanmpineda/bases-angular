93aef11946119be439d551ea66c10bd0a3822639
Dilan Martinez <dilanmpineda@gmail.com>
2021-10-05 19:39:34 -0300

    bases

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+  heroes: string[] = ['Ironman', 'Spiderman', 'Hulk', 'Goku'];
+  heroeBorrado:string = '';
+  borrarHeroe(): void {
+    this.heroeBorrado = this.heroes.pop() || '';
+  }
+}
